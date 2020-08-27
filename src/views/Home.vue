89a9a5449ebed0bb6c71eb181d00fd3d2095ab27
ggx <abc123>
2020-08-27 20:04:47 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <router-link to="/local">本地连接例子</router-link> |<br />
       <router-link to="/remote">远程连接视频</router-link> |<br />
-
       <router-link to="/some">多人连接例子</router-link>
     </div>
   </div>
