38090d456c3bfbac2c96d5682eed2be352bcad4f
ggx <abc123>
2021-10-20 15:20:53 +0800

    feat: 优化

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client'
-let host = 'http://192.168.1.101:3002'
+// let host = 'http://192.168.1.101:3002'
+let host = 'http://172.16.0.108:3002'
 const socket  = io.connect(host)
 export default socket
